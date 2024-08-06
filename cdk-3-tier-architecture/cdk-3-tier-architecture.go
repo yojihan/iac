e49a53bcbc9fe27f22864f749d6ebeaccad3fe42
yojihan <141936250+yojihan@users.noreply.github.com>
2024-08-06 05:41:29 +0000

    edit to one stack structure

--- cdk-3-tier-architecture/cdk-3-tier-architecture.go
+++ cdk-3-tier-architecture/cdk-3-tier-architecture.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
-
 	"cdk-3-tier-architecture/vpc"
+	"fmt"
 
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/jsii-runtime-go"
 )
+
+type Cdk3TeirStackProps struct {
+	awscdk.StackProps
+}
 
 func main() {
 	defer jsii.Close()
 
 	app := awscdk.NewApp(nil)
 
-	// vpc
-	vpc.NewStack(app, "VPC", &vpc.VPCStackProps{
-		StackProps: awscdk.StackProps{
-			Env: &awscdk.Environment{
-				Region: jsii.String("ap-northeast-1"),
-			},
+	cdk3TeirStack := awscdk.NewStack(app, jsii.String("Stack"), &awscdk.StackProps{
+		Env: &awscdk.Environment{
+			Region: jsii.String("ap-northeast-1"),
 		},
 	})
+
+	vpc1 := vpc.NewVPC(cdk3TeirStack, jsii.String("Vpc"), &awsec2.VpcProps{
+		IpAddresses: awsec2.IpAddresses_Cidr(jsii.String("10.0.0.0/24")),
+		MaxAzs:      jsii.Number(2),
+	})
+	fmt.Println(vpc1)
 
 	app.Synth(nil)
 }
--- cdk-3-tier-architecture/vpc/vpc.go
+++ cdk-3-tier-architecture/vpc/vpc.go
@@ -1,39 +1,10 @@
 package vpc
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsssm"
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
 )
 
-type VPCStackProps struct {
-	awscdk.StackProps
-}
-
-func NewStack(scope constructs.Construct, id string, props *VPCStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-	cidr := "10.0.0.0/24"
-
-	vpc := awsec2.NewVpc(stack, jsii.String("3tier-vpc"),
-		&awsec2.VpcProps{
-			IpAddresses: awsec2.IpAddresses_Cidr(&cidr),
-			MaxAzs:      jsii.Number(1),
-		},
-	)
-
-	awsssm.NewStringParameter(stack, jsii.String("3tier-vpc-parameter"),
-		&awsssm.StringParameterProps{
-			Description:   jsii.String("Create VPC"),
-			ParameterName: jsii.String("/network/3tier-vpc"),
-			StringValue:   vpc.VpcId(),
-		},
-	)
-
-	return stack
+func NewVPC(scope constructs.Construct, id *string, props *awsec2.VpcProps) awsec2.Vpc {
+	return awsec2.NewVpc(scope, id, props)
 }

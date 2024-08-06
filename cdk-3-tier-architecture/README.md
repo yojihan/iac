766607b45f37ab5d810dffdf62138a3df13872ec
yojihan <141936250+yojihan@users.noreply.github.com>
2024-08-06 00:42:36 +0000

    init cdk project

---
+++ cdk-3-tier-architecture/README.md
@@ -0,0 +1,18 @@
+# CDK (3 tier Architecture)
+
+## Architecture
+
+![Architecture](./img/architecture.png)
+
+## Environment
+
+- Go 1.22.5
+- AWS CLI 2.17.12
+- AWS CDK CLI 2.148.1
+
+## Init Project
+
+```bash
+$ cdk init --language go
+$ go get
+```
---
+++ cdk-3-tier-architecture/cdk-3-tier-architecture_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in cdk-3-tier-architecture_test.go
+// func TestCdk3TierArchitectureStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewCdk3TierArchitectureStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack, nil)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }

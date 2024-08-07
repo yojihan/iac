51d780df2c6773d2673492903a8f4a55d4b10fa6
yojihan <141936250+yojihan@users.noreply.github.com>
2024-08-07 06:11:04 +0000

    add tags

--- cdk-3-tier-architecture/cdk-3-tier-architecture.go
+++ cdk-3-tier-architecture/cdk-3-tier-architecture.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/jsii-runtime-go"
@@ -73,12 +71,14 @@ func main() {
 		CidrBlock:        jsii.String("10.0.0.0/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1A),
 	})
+	awscdk.Tags_Of(natSubnet).Add(jsii.String("Name"), jsii.String("NAT-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	bastionSubnet := awsec2.NewSubnet(cdk3TierStack, jsii.String("BastionSubnet"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.16/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1C),
 	})
+	awscdk.Tags_Of(bastionSubnet).Add(jsii.String("Name"), jsii.String("bastion-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	// private subnets
 	webSubnet1 := awsec2.NewSubnet(cdk3TierStack, jsii.String("WebSubnet1"), &awsec2.SubnetProps{
@@ -86,38 +86,42 @@ func main() {
 		CidrBlock:        jsii.String("10.0.0.32/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1A),
 	})
+	awscdk.Tags_Of(webSubnet1).Add(jsii.String("Name"), jsii.String("web1-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	webSubnet2 := awsec2.NewSubnet(cdk3TierStack, jsii.String("WebSubnet2"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.48/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1C),
 	})
+	awscdk.Tags_Of(webSubnet2).Add(jsii.String("Name"), jsii.String("web2-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	wasSubnet1 := awsec2.NewSubnet(cdk3TierStack, jsii.String("WasSubnet1"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.64/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1A),
 	})
+	awscdk.Tags_Of(wasSubnet1).Add(jsii.String("Name"), jsii.String("was1-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	wasSubnet2 := awsec2.NewSubnet(cdk3TierStack, jsii.String("WasSubnet2"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.80/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1C),
 	})
+	awscdk.Tags_Of(wasSubnet2).Add(jsii.String("Name"), jsii.String("was2-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	rdsSubnet1 := awsec2.NewSubnet(cdk3TierStack, jsii.String("RDSSubnet1"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.96/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1A),
 	})
+	awscdk.Tags_Of(rdsSubnet1).Add(jsii.String("Name"), jsii.String("rds1-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	rdsSubnet2 := awsec2.NewSubnet(cdk3TierStack, jsii.String("RDSSubnet2"), &awsec2.SubnetProps{
 		VpcId:            vpc.VpcId(),
 		CidrBlock:        jsii.String("10.0.0.112/28"),
 		AvailabilityZone: jsii.String(AZ_AP_NORTHEAST_1C),
 	})
-
-	fmt.Println(natSubnet, bastionSubnet, webSubnet1, webSubnet2, wasSubnet1, wasSubnet2, rdsSubnet1, rdsSubnet2)
+	awscdk.Tags_Of(rdsSubnet2).Add(jsii.String("Name"), jsii.String("rds2-subnet-cdk-3tier"), &awscdk.TagProps{})
 
 	app.Synth(nil)
 }
